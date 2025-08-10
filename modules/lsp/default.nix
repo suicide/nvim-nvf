db33105e18729a4b523729aa06b85d63fcc4656a
suiiii <suiiii@suiiii.rip>
2025-08-10 23:59:52 +0200

    feat(format): LSP formatting key remapped

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -4,38 +4,38 @@
   config,
   ...
 }: {
-
   imports = [];
   config = {
-
     vim = {
+      lsp = {
+        # Enable LSP functionality globally. This is required for modules found
+        # in `vim.languages` to enable relevant LSPs.
+        enable = true;
 
-          lsp = {
-            # Enable LSP functionality globally. This is required for modules found
-            # in `vim.languages` to enable relevant LSPs.
-            enable = true;
+        # You may define your own LSP configurations using `vim.lsp.servers` in
+        # nvf without ever needing lspconfig to do it. This will use the native
+        # API provided by Neovim > 0.11
+        servers = {};
 
-            # You may define your own LSP configurations using `vim.lsp.servers` in
-            # nvf without ever needing lspconfig to do it. This will use the native
-            # API provided by Neovim > 0.11
-            servers = {};
-          };
+        mappings = {
+          format = "<leader>F";
+        };
+      };
 
-          # Language support and automatic configuration of companion plugins.
-          # Note that enabling, e.g., languages.<lang>.diagnostics will automatically
-          # enable top-level options such as enableLSP or enableExtraDiagnostics as
-          # they are needed.
-          languages = {
-            enableFormat = true;
-            enableTreesitter = true;
-            enableExtraDiagnostics = true;
+      # Language support and automatic configuration of companion plugins.
+      # Note that enabling, e.g., languages.<lang>.diagnostics will automatically
+      # enable top-level options such as enableLSP or enableExtraDiagnostics as
+      # they are needed.
+      languages = {
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
 
-            # Nix language and diagnostics.
-            nix = {
-              enable = true;
-            };
-
-          };
+        # Nix language and diagnostics.
+        nix = {
+          enable = true;
+        };
+      };
     };
   };
 }
