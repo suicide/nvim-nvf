2ad0e85f4fbdd971fe4fe455ea2907c884e5054a
suiiii <suiiii@suiiii.rip>
2025-09-06 20:45:14 +0200

    style(lualine): Styling lualine

--- modules/lualine.nix
+++ modules/lualine.nix
@@ -10,35 +10,80 @@
     vim = {
 
       statusline = let
-        activeZ = options.vim.statusline.lualine.activeSection.z.default ;
+        activeB = options.vim.statusline.lualine.activeSection.b.default ;
       in {
         lualine = {
           enable = true;
 
-          # activeSection.a = [
-          #   ''
-          #   'encoding', 'fileformat', 'filetype'
-          #   ''
-          # ];
-          #
-          activeSection.c = [
+          activeSection.a = [
             ''
-            {
-              'diff',
-            }
+              {
+               "mode",
+               icons_enabled = true,
+              }
             ''
           ];
 
-          activeSection.z = activeZ 
-            ++
-          [
+          activeSection.b = [
             ''
+              {
+                'branch',
+                -- separator = {right = ''}
+              }
+            ''
+            ''
+              {
+                'diff',
+                -- separator = {right = ''}
+              },
+            ''
+          ];
+
+          activeSection.c = let
+            firstEntry = builtins.head activeB;
+          in  [
+          firstEntry
+          ''
+            {
+              "filename",
+              path = 1,
+              symbols = {modified = ' ', readonly = ' '},
+              -- separator = {right = ''}
+            }
+          ''
+          ];
+
+          activeSection.y = [
+          ''
             {
               'encoding',
+              -- separator = {left = ''}
             }
-            ''
-          ]
-          ;
+          ''
+          ''
+            {
+              "fileformat",
+              -- color = {fg='black'},
+              symbols = {
+                unix = '', -- e712
+                dos = '',  -- e70f
+                mac = '',  -- e711
+              }
+            }
+          ''
+          ];
+
+          activeSection.z = [
+          ''
+            {
+              "progress",
+              -- separator = {left = ''}
+            }
+          ''
+          ''
+            {"location"}
+          ''
+          ];
 
           inactiveSection.c = [
             ''
@@ -49,6 +94,17 @@
             ''
           ];
 
+          sectionSeparator = {
+            left = "";
+            right = "";
+          };
+
+          componentSeparator = {
+            left = "";
+            right = "";
+          };
+
+          # line on each window
           globalStatus = false;
 
           refresh = {
