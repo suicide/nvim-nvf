89110793d02c427fea6c9031f93dd6fe5943068a
suiiii <suiiii@suiiii.rip>
2025-09-19 17:35:48 +0200

    feat(filetree): Enabled Oil
    In favor of neo-tree

--- modules/filetree.nix
+++ modules/filetree.nix
@@ -8,39 +8,79 @@
 
   config = {
     vim = {
-      filetree.neo-tree = {
-        enable = true;
 
-        setupOpts = {
-          filesystem = {
-            hijack_netrw_behavior = "open_current";
+      utility = {
+        oil-nvim = {
+          enable = true;
 
-            filtered_items = {
-              hide_dotfiles = false;
-              hide_gitignored = false;
-              hide_hidden = false;
+          setupOpts = {
+
+            columns = [
+              "size"
+              "icon"
+            ];
+
+            view_options = {
+              show_hidden = true;
+            };
+
+            keymaps = {
+              "<C-p>" = false; # we want to use telescope file finder instead
+              "<M-p>" = "actions.preview";
             };
           };
-
-          git_status_async = true;
         };
       };
 
-      lazy.plugins."neo-tree-nvim".keys = lib.mkIf config.vim.filetree.neo-tree.enable [
+      keymaps = lib.mkIf config.vim.utility.oil-nvim.enable [
         {
           mode = "n";
-          key = "<C-t>";
-          action = "<cmd>Neotree toggle<cr>";
-          desc = "Toggle Neotree";
-        }
-
-        {
-          mode = "n";
-          key = "<C-f>";
-          action = "<cmd>Neotree reveal<cr>";
-          desc = "Find in Neotree";
+          key = "-";
+          action = "<cmd>Oil<cr>";
+          desc = "Oil parent";
         }
       ];
+
+
+      filetree = {
+        neo-tree = {
+          enable = false;
+
+          setupOpts = {
+            filesystem = {
+              hijack_netrw_behavior = "open_current";
+
+              filtered_items = {
+                hide_dotfiles = false;
+                hide_gitignored = false;
+                hide_hidden = false;
+              };
+            };
+
+            git_status_async = true;
+          };
+        };
+      };
+
+      lazy.plugins = lib.mkIf config.vim.filetree.neo-tree.enable {
+        "neo-tree-nvim" = {
+          keys = [
+            {
+              mode = "n";
+              key = "<C-t>";
+              action = "<cmd>Neotree toggle<cr>";
+              desc = "Toggle Neotree";
+            }
+
+            {
+              mode = "n";
+              key = "<C-f>";
+              action = "<cmd>Neotree reveal<cr>";
+              desc = "Find in Neotree";
+            }
+          ];
+        };
+      };
     };
   };
 }
