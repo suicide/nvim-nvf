49d57b4fad3d6433ecef04197185886c8c273da0
suiiii <suiiii@suiiii.rip>
2025-09-25 01:29:00 +0200

    feat(dap): Added dap virtual text

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -82,6 +82,16 @@
         };
       };
 
+      extraPlugins = {
+        "nvim-dap-virtual-text" = {
+          package = pkgs.vimPlugins.nvim-dap-virtual-text;
+          after = [ "nvim-dap" ];
+          setup = ''
+            require("nvim-dap-virtual-text").setup()
+          '';
+        };
+      };
+
       keymaps = [
         {
           mode = "n";
