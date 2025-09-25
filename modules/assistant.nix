4dd244e2a4569bbbfc8e162fa3bcad567af0735c
suiiii <suiiii@suiiii.rip>
2025-09-25 18:14:45 +0200

    fix(assitant): Remapped refresh keymap to avoid collision

--- modules/assistant.nix
+++ modules/assistant.nix
@@ -5,18 +5,21 @@
   options,
   ...
 }: {
-
   config = {
     vim = {
       assistant.copilot = {
         enable = true;
+
+        mappings = {
+          panel = {
+            refresh = "ggr";
+          };
+        };
       };
 
       lazy.plugins.blink-copilot = {
         package = pkgs.vimPlugins.blink-copilot;
       };
-
     };
   };
 }
-
