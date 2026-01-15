bf08f656263e7e056264c6150adc11d5fd8024ea
suiiii <suiiii@suiiii.rip>
2026-01-15 17:07:31 +0100

    fix(svelte): treesitter highlighting workaround

--- modules/lsp/svelte.nix
+++ modules/lsp/svelte.nix
@@ -5,12 +5,15 @@
   options,
   ...
 }: {
-
   config = {
     vim = {
       languages.svelte = {
         enable = true;
       };
+
+      # temporary fix for proper highlighting of svelte files
+      # see https://github.com/NotAShelf/nvf/pull/1315#issuecomment-3728427584
+      startPlugins = with pkgs.vimPlugins.nvim-treesitter.queries; [ecma html_tags jsx];
     };
   };
 }
