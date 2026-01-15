e635d617f8fc93cb00d795ac370384dc50ea3d99
suiiii <suiiii@suiiii.rip>
2026-01-15 22:52:10 +0100

    fix(git): Workaround for git commit highlighting
    Refs: NotAShelf/nvf#1315

--- modules/gitsigns.nix
+++ modules/gitsigns.nix
@@ -20,12 +20,23 @@
 
       treesitter = {
         grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          git_config
           git_rebase
           gitattributes
           gitcommit
           gitignore
         ];
       };
+
+      # temporary fix for proper highlighting of svelte files
+      # see https://github.com/NotAShelf/nvf/pull/1315#issuecomment-3728427584
+      startPlugins = with pkgs.vimPlugins.nvim-treesitter.queries; [
+        git_config
+        git_rebase
+        gitattributes
+        gitcommit
+        gitignore
+      ];
     };
   };
 }
