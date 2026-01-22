296c8db7edb057d48c1fd020642a3aeb0d2fc092
suiiii <suiiii@suiiii.rip>
2026-01-22 17:09:01 +0100

    chore(treesitter): Remove workarounds
    upstream fix in nixpkgs

--- modules/gitsigns.nix
+++ modules/gitsigns.nix
@@ -28,15 +28,6 @@
         ];
       };
 
-      # temporary fix for proper highlighting of svelte files
-      # see https://github.com/NotAShelf/nvf/pull/1315#issuecomment-3728427584
-      startPlugins = with pkgs.vimPlugins.nvim-treesitter.queries; [
-        git_config
-        git_rebase
-        gitattributes
-        gitcommit
-        gitignore
-      ];
     };
   };
 }
