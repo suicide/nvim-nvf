992db5df4b238d3907f0046acb45d76546fe5b3b
suiiii <suiiii@suiiii.rip>
2026-03-02 23:35:36 +0100

    fix: removed deprecation warning for `system` usage

--- modules/assistant/mcphub/default.nix
+++ modules/assistant/mcphub/default.nix
@@ -10,10 +10,10 @@
     vim = lib.mkIf config.vim.assistant.codecompanion-nvim.enable {
       lazy.plugins = {
         "mcphub.nvim" = {
-          package = flakeInputs.mcphub-nvim.packages.${pkgs.system}.default;
+          package = flakeInputs.mcphub-nvim.packages.${pkgs.stdenv.hostPlatform.system}.default;
           setupModule = "mcphub";
           setupOpts = {
-            cmd = "${flakeInputs.mcphub.packages.${pkgs.system}.default}/bin/mcp-hub";
+            cmd = "${flakeInputs.mcphub.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/mcp-hub";
           };
         };
       };
